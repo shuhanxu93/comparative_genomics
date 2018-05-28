e564d6050b01eef391beaba6b6062b24b43cc125
shuhanxu93 <shuhan.xu@stud.ki.se>
2018-05-28 14:40:33 +0200

    add

--- genomes/genome.py
+++ genomes/genome.py
@@ -6,7 +6,7 @@ def complement(sequence):
     sequence_com = sequence.replace('A', 't').replace('C', 'g').replace('G', 'c').replace('T', 'a').upper()
     return sequence_com
 
-test = genome[:10000]
+test = genome[:500000]
 f1_met = []
 f2_met = []
 f3_met = []
@@ -14,54 +14,108 @@ r1_met = []
 r2_met = []
 r3_met = []
 
-f1_stop = []
-f2_stop = []
-f3_stop = []
+f1_stop = [-3]
+f2_stop = [-3]
+f3_stop = [-3]
 r1_stop = []
 r2_stop = []
 r3_stop = []
 
-for index in range(0, len(test), 3):
-    codon = test[index : index + 3]
+for coordinate in range(0, len(test), 3):
+    codon = test[coordinate : coordinate + 3]
     if codon == 'ATG':
-        f1_met.append(index)
-    if codon == 'TAC':
-        r1_met.append(index + 2)
+        f1_met.append(coordinate)
+    if codon == 'CAT':
+        r1_met.append(coordinate + 2)
     if codon == 'TAA' or codon == 'TAG' or codon == 'TGA':
-        f1_stop.append(index)
-    if codon == 'ATT' or codon == 'ATC' or codon == 'ACT':
-        r1_stop.append(index + 2)
+        f1_stop.append(coordinate)
+    if codon == 'TTA' or codon == 'CTA' or codon == 'TCA':
+        r1_stop.append(coordinate + 2)
 
-for index in range(1, len(test), 3):
-    codon = test[index : index + 3]
+for coordinate in range(1, len(test), 3):
+    codon = test[coordinate : coordinate + 3]
     if codon == 'ATG':
-        f2_met.append(index)
-    if codon == 'TAC':
-        r2_met.append(index + 2)
+        f2_met.append(coordinate)
+    if codon == 'CAT':
+        r2_met.append(coordinate + 2)
     if codon == 'TAA' or codon == 'TAG' or codon == 'TGA':
-        f2_stop.append(index)
-    if codon == 'ATT' or codon == 'ATC' or codon == 'ACT':
-        r2_stop.append(index + 2)
+        f2_stop.append(coordinate)
+    if codon == 'TTA' or codon == 'CTA' or codon == 'TCA':
+        r2_stop.append(coordinate + 2)
 
-for index in range(2, len(test), 3):
-    codon = test[index : index + 3]
+for coordinate in range(2, len(test), 3):
+    codon = test[coordinate : coordinate + 3]
     if codon == 'ATG':
-        f3_met.append(index)
-    if codon == 'TAC':
-        r3_met.append(index + 2)
+        f3_met.append(coordinate)
+    if codon == 'CAT':
+        r3_met.append(coordinate + 2)
     if codon == 'TAA' or codon == 'TAG' or codon == 'TGA':
-        f3_stop.append(index)
-    if codon == 'ATT' or codon == 'ATC' or codon == 'ACT':
-        r3_stop.append(index + 2)
+        f3_stop.append(coordinate)
+    if codon == 'TTA' or codon == 'CTA' or codon == 'TCA':
+        r3_stop.append(coordinate + 2)
 
-glist_1 = []
-for index in range(len(f1_stop) - 1):
-    if f1_stop[index + 1] - f1_stop[index] >= 113:
-        for coordinate in f1_met:
-            if coordinate > f1_stop[index] and coordinate < f1_stop[index + 1]:
-                if f1_stop[index + 1] - coordinate >= 113:
-                    glist_1.append([coordinate, f1_stop[index + 1]])
-                    break
-                if coordinate > f1_stop[index + 1]:
-                    break
-print(glist_1)
+gene_list = []
+for index in range(1, len(f1_stop)):
+    stop_coord = f1_stop[index]
+    prev_stop_coord = f1_stop[index - 1]
+    if stop_coord - prev_stop_coord >= 113:
+        for start_coord in f1_met:
+            if start_coord > prev_stop_coord:
+                if stop_coord - start_coord >= 110:
+                    gene_list.append([start_coord, stop_coord, 1])
+                break
+
+for index in range(1, len(f2_stop)):
+    stop_coord = f2_stop[index]
+    prev_stop_coord = f2_stop[index - 1]
+    if stop_coord - prev_stop_coord >= 113:
+        for start_coord in f2_met:
+            if start_coord > prev_stop_coord:
+                if stop_coord - start_coord >= 110:
+                    gene_list.append([start_coord, stop_coord, 2])
+                break
+
+for index in range(1, len(f3_stop)):
+    stop_coord = f3_stop[index]
+    prev_stop_coord = f3_stop[index - 1]
+    if stop_coord - prev_stop_coord >= 113:
+        for start_coord in f3_met:
+            if start_coord > prev_stop_coord:
+                if stop_coord - start_coord >= 110:
+                    gene_list.append([start_coord, stop_coord, 3])
+                break
+
+r1_stop.append(len(test) + 2)
+for index in range(len(r1_stop) - 1):
+    stop_coord = r1_stop[index]
+    prev_stop_coord = r1_stop[index + 1]
+    if prev_stop_coord - stop_coord >= 113:
+        for start_coord in r1_met[::-1]:
+            if start_coord < prev_stop_coord:
+                if start_coord - stop_coord >= 110:
+                    gene_list.append([stop_coord, start_coord, -1])
+                break
+
+r2_stop.append(len(test) + 2)
+for index in range(len(r2_stop) - 1):
+    stop_coord = r2_stop[index]
+    prev_stop_coord = r2_stop[index + 1]
+    if prev_stop_coord - stop_coord >= 113:
+        for start_coord in r2_met[::-1]:
+            if start_coord < prev_stop_coord:
+                if start_coord - stop_coord >= 110:
+                    gene_list.append([stop_coord, start_coord, -2])
+                break
+
+r3_stop.append(len(test) + 2)
+for index in range(len(r3_stop) - 1):
+    stop_coord = r3_stop[index]
+    prev_stop_coord = r3_stop[index + 1]
+    if prev_stop_coord - stop_coord >= 113:
+        for start_coord in r3_met[::-1]:
+            if start_coord < prev_stop_coord:
+                if start_coord - stop_coord >= 110:
+                    gene_list.append([stop_coord, start_coord, -3])
+                break
+
+print(len(gene_list))
