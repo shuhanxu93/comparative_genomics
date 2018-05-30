ce73aa5d4c0ef90b10af14a14c4fc8c94a91d42c
shuhanxu93 <shuhan.xu@stud.ki.se>
2018-05-30 17:59:06 +0200

    add

--- project/scripts/statistics.py
+++ project/scripts/statistics.py
@@ -5,11 +5,11 @@ import collections
 organisms = sys.argv[1:]
 
 gc_freq = np.zeros(len(organisms))
-for index in range(len(organisms)):
-    with open(organisms[index], 'r') as filehandle:
+for org_index in range(len(organisms)):
+    with open(organisms[org_index], 'r') as filehandle:
         genome = filehandle.read().splitlines()[1]
         cnt = collections.Counter(genome)
-        gc_freq[index] = (cnt['G'] + cnt ['C']) / sum(cnt.values())
+        gc_freq[org_index] = (cnt['G'] + cnt ['C']) / len(genome)
 
 nucleotides = ['A', 'C', 'G', 'T']
 dinucleotides = []
@@ -18,4 +18,12 @@ for nucleotide1 in nucleotides:
         dinucleotides.append(nucleotide1 + nucleotide2)
 
 dinucl_freq = np.zeros((len(organisms), len(dinucleotides)))
-print(dinucl_freq)
+for org_index in range(len(organisms)):
+    with open(organisms[org_index], 'r') as filehandle:
+        genome = filehandle.read().splitlines()[1]
+        dinucl_list = []
+        for gen_index in range(len(genome) - 1):
+            dinucl_list.append(genome[gen_index : gen_index + 2])
+        cnt = collections.Counter(dinucl_list)
+        for dinucl_index in range(len(dinucleotides)):
+            dinucl_freq[org_index, dinucl_index] = cnt[dinucleotides[dinucl_index]] / (len(genome) - 1)
