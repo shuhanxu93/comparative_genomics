011de0a565c852fb6b56b88f9e5e0bdccb270dbb
FuqiXu <itisfuqixu@gmail.com>
2018-05-31 16:00:54 +0200

    update

--- project/blast_result/truePredCounter.py
+++ project/blast_result/truePredCounter.py
@@ -4,27 +4,32 @@ inFile1 = sys.argv[1]
 inFile2 = sys.argv[2]
 
 #threshold = 10**-3
-with open(inFile1) as f:
+with open(inFile1) as f1:
     pairs = []
-    hits = f.readlines()
+    hits = f1.readlines()
     for lines in hits:
             pairs.append([lines.split()[0],lines.split()[1]])
-    
+    '''
     forward = set()
     for i in pairs:
         forward.add((i[0],i[1]))
     print("size of " + str(inFile1) +'\n'+str(len(set(forward))))
-
-with open(inFile2) as f:
+    '''
+with open(inFile2) as f2:
     pairs_rev = []
-    hits = f.readlines()
-    for lines in hits:
+    hits2 = f2.readlines()
+    for lines in hits2:
             pairs_rev.append([lines.split()[1],lines.split()[0]])
-    
+    '''
     rev = set()
     for i in pairs:
         rev.add((i[0],i[1]))
     print("size of " + str(inFile2) +'\n'+str(len(set(forward))))
-    
-print("size of intersection of " + str(inFile1) +'\n'+str(len(set(forward).intersection(rev))))
+    '''
+count = 0    
+for i in pairs:
+    if i in pairs_rev:
+        count = count+1
+
+print("size of intersection of " + str(inFile1) +'\n'+str(count))
 
