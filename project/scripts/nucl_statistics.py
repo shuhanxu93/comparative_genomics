c36508001d9bba857f6c43a4389d67105d20f55e
shuhanxu93 <shuhan.xu@stud.ki.se>
2018-05-31 12:09:16 +0200

    add

--- project/scripts/nucl_statistics.py
+++ project/scripts/nucl_statistics.py
@@ -15,13 +15,14 @@ for nucleotide1 in nucleotides:
 gc_freq = np.zeros(len(organisms))
 nucl_freq = np.zeros((len(organisms), len(nucleotides)))
 dinucl_freq = np.zeros((len(organisms), len(dinucleotides)))
+
 for org_index in range(len(organisms)):
     with open(organisms[org_index], 'r') as filehandle:
+
         genome = filehandle.read().splitlines()[1]
+
         cnt = collections.Counter(genome)
-
         gc_freq[org_index] = (cnt['G'] + cnt ['C']) / len(genome)
-
         for nucl_index in range(len(nucleotides)):
             nucl_freq[org_index, nucl_index] = cnt[nucleotides[nucl_index]] / len(genome)
 
@@ -36,6 +37,6 @@ df_gc_freq = pd.DataFrame(data=gc_freq, index=organisms, columns=['GC'])
 df_nucl_freq = pd.DataFrame(data=nucl_freq, index=organisms, columns=nucleotides)
 df_dinucl_freq = pd.DataFrame(data=dinucl_freq, index=organisms, columns=dinucleotides)
 
-df_gc_freq.to_csv('gc_frequency.csv', sep=',', encoding='utf-8')
-df_nucl_freq.to_csv('nucleotide_frequency.csv', sep=',', encoding='utf-8')
-df_dinucl_freq.to_csv('dinucleotide_frequency.csv', sep=',', encoding='utf-8')
+df_gc_freq.to_csv('gc_freq.csv', sep=',', encoding='utf-8')
+df_nucl_freq.to_csv('nucl_freq.csv', sep=',', encoding='utf-8')
+df_dinucl_freq.to_csv('dinucl_freq.csv', sep=',', encoding='utf-8')
