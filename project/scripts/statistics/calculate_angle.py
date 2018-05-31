c89902d0a0a0bb23b0e3f7204f4639b57513bcd1
shuhanxu93 <shuhan.xu@stud.ki.se>
2018-05-31 21:40:15 +0200

    add

--- project/scripts/statistics/calculate_angle.py
+++ project/scripts/statistics/calculate_angle.py
@@ -12,7 +12,7 @@ data = df.values[:,1:]
 distances = np.zeros((len(organisms), len(organisms)))
 for i in range(len(organisms)):
     for j in range(len(organisms)):
-        distances[i, j] = np.sqrt(np.sum((data[i] - data[j]) ** 2))
+        distances[i, j] = np.dot(data[i], data[j])
 
 print(' '.join(map(str, organisms)))
 for row in distances:
