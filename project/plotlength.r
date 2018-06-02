f5e43313062dec4bbef788daccb4f1d3aa7c71a6
FuqiXu <36501176+FuqiXu@users.noreply.github.com>
2018-06-02 06:59:25 +0200

    update

--- project/plotlength.r
+++
@@ -1,8 +0,0 @@
-setwd('~/comparative_genomics/project/scripts/glimmer_evaluation')
-library(ggplot2)
-plotGlimmer = ​ function​ (file=​ '51.glimmer.predict'​ ) {
-t = read.table(file, header = F, skip = ​ 1 ​ )
-c = data.frame(size=abs(t[,​ 2 ​ ]-t[,​ 3 ​ ]))
-ggplot(c, aes(size))+geom_histogram(binwidth=​ 50​ )+ggtitle(file)
-}
-plotGlimmer()
